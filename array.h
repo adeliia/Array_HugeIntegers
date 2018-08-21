03970bebb274cc3e0fb43b6d4a63e134c85c371c
adeliia <adeliia.gataullina@gmail.com>
2018-08-21 22:59:41 +0200

    first commit

---
+++ array.h
@@ -0,0 +1,31 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class Array {
+    friend ostream &operator<<( ostream &, const Array &);
+    friend istream &operator>>( istream &, Array &);
+public:
+    Array(int = 10);
+    Array(const Array &);
+    ~Array();
+    int getSize() const;
+
+    const Array &operator=(const Array &);
+    bool operator==( const Array &) const;
+
+    bool operator!=( const Array &right) const {
+        return ! (*this == right);
+    }
+
+    int &operator[](int);
+    int operator[](int) const;
+private:
+    int size;
+    int *ptr;
+};
+
+#endif // ARRAY_H
